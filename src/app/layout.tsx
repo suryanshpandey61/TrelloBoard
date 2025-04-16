9d3aa3815d1c3b68e77720c7cda7ef755094947e
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-04-16 12:34:58 +0530

    Upation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-gray-200 text-gray-900">{children}</body>
+      <body className="bg-gray-100 text-gray-900">{children}</body>
     </html>
   )
 }
