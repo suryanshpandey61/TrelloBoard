9d292c1f90cb3c499ce64546dd97e7931ff516ea
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-04-23 23:03:45 +0530

    context updated

--- src/app/context/AuthContext.tsx
+++ src/app/context/AuthContext.tsx
@@ -1,18 +1,32 @@
 'use client'
+
 import { createContext, useContext, useEffect, useState } from 'react'
-import {}
+import {jwtDecode} from 'jwt-decode'
 import { useRouter } from 'next/navigation'
 
-const AuthContext = createContext(null)
+type User = {
+  id: number
+  name: string
+  email: string
+  iat?: number
+  exp?: number
+}
+
+type AuthContextType = {
+  user: User | null
+  logout: () => void
+}
+
+const AuthContext = createContext<AuthContextType | null>(null)
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState<User | null>(null)
   const router = useRouter()
 
   useEffect(() => {
     const token = localStorage.getItem('token')
     if (token) {
-      const decoded = jwtDecode(token)
+      const decoded = jwtDecode<User>(token)
       setUser(decoded)
     }
   }, [])
@@ -30,4 +44,10 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   )
 }
 
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
